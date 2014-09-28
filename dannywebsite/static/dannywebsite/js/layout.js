6e6e7e147974245ac8c5f4a625050c4aecd7e33f
Danny Milsom <danny.milsom@cgi.com>
2014-09-28 21:47:25 +0100

    Tidies up JS and removes technology logo from homepage
    * Use a more OOP approach to JavaScript
    * Prefer the use of classes instead of inline styles applied to DOM via JavaScript
    * Changed click handlers to use on('click')
    * Changed click event on submit button to form submission listener
    * Removed technology logos from homepage (e.g. Python) - we already have enough images
      without cluttering the UI further

--- dannywebsite/static/dannywebsite/js/layout.js
+++ dannywebsite/static/dannywebsite/js/layout.js
@@ -1,89 +1,91 @@
-$(document).ready(function () {
+(function($) {
 
-    $("#site-search").select2({
-      placeholder: 'Find a blog post'
-    });
+  var layout = {
+    init: function() {
 
-    $('#nav_blog').click( function() {
-        $('#hidden_nav_container').attr('display', 'inline');
-    });
+      this.prepareDOM();
+      this.bindListeners();
 
-    $("#contact-form-submit-btn").click(function(e) {
-        e.preventDefault();
-        $.ajax({
-            url: '',
-            type: 'POST',
-            dataType: 'json',
-            crossDomain: false, // needed by is_ajax() server side
-            data: $("#contact-us-form").serialize(),
-            success: function(data) {
-              if (data['errors']) {
-                $("#warning-message, #warning-list").remove();
-                $("input, textarea").removeClass('validation-warning');
+    },
+    prepareDOM: function() {
 
-                for (m in data['errors']) {
-                  if (m === 'email') {
-                    $("#id_email").val("");
-                  }
-                  $("#id_" + m).addClass('validation-warning')
-                               .attr('placeholder', m + " - " + data['errors'][m]);
-                }
-              } else {
-                $( "#contact-us-form" ).slideUp( "slow");
-                $( "#contact-us-success").removeClass("hide-it");
-                $( "#contact-us-success").slideDown( "slow");
-                $("#success-message").html(data['message'])
-              }
-            }
-        });
-    })
-
-    if (matchMedia('only screen and (min-width: 767px)').matches) {
-
-      $(".skill-category").click(function(e) {
-        e.stopPropagation();
-        if ($(this).hasClass("active")) {
-          // we're looking at a category and want to hide the images
-          $(".skill-text", this).css("display", "none");
-          $("#portfolio .skills-img").css("width", "40%")
-          $("#portfolio").css("opacity", "1");
-          $('.skill-info').addClass("hide-it"); 
-          $(this).removeClass("active");
-          $(".skill-category").removeClass("focused");
-          $("#portfolio").removeClass("focused");
-        }
-        else {
-          // we want to look at a new category
-          // first thing we do is make sure all skill rows are hidden
-          $(".skill-text").css("display", "none");
-          $("#portfolio").css("opacity", "0")
-          $("#portfolio .skills-img").css("width", "20%")
-          $('.skill-info').addClass("hide-it");
-          // and that all the category images have opacity 1
-          $(".skill-category").removeClass("focused")
-          $(".skill-category").removeClass("active")
-
-          // Reveal skill and blur out other categories
-          $(".skill-text", this).css("display", "block");
-          $(".skill-category").addClass("focused");
-          $(this).addClass("active");
-          $('.' + $(this).attr('id')).removeClass("hide-it"); 
-        }
+      $("#site-search").select2({
+        placeholder: 'Find a blog post'
       });
+
+    },
+    bindListeners: function() {
+
+      if (matchMedia('only screen and (min-width: 767px)').matches) {
+
+        $(".skill-category").click(function(e) {
+          e.stopPropagation();
+          if ($(this).hasClass("active")) {
+            // hide text
+            $(".skill-text").removeClass("show-skills-text");
+
+            // remove inline margin
+            $("#portfolio").css("margin-top", "");
+
+            // remove focused and active classes
+            $(".skill-category").removeClass("focused");
+            $(this).removeClass("active");
+          }
+          else {
+            // make sure all skill rows are hidden
+            $(".skill-text").removeClass("show-skills-text");
+
+            // calculate how much margin we need for the text
+            $("#portfolio").css("margin-top", $(".skill-text", this).innerHeight());
+
+            // ensure all the category images have opacity 1
+            $(".skill-category").removeClass("focused active")
+
+            // Reveal skill and blur out other categories
+            $(".skill-text", this).addClass("show-skills-text")
+            $(".skill-category").addClass("focused");
+            $(this).addClass("active");
+          }
+        });
+      }
+
+      $("#contact-us-form").submit(function() {
+          e.preventDefault();
+          $.ajax({
+              type: 'POST',
+              dataType: 'json',
+              crossDomain: false, // needed by is_ajax() server side
+              data: $(this).serialize(),
+              success: function(data) {
+                if (data['errors']) {
+                  $("#warning-message, #warning-list").remove();
+                  $("input, textarea").removeClass('validation-warning');
+
+                  for (m in data['errors']) {
+                    if (m === 'email') {
+                      $("#id_email").val("");
+                    }
+                    $("#id_" + m).addClass('validation-warning')
+                                 .attr('placeholder', m + " - " + data['errors'][m]);
+                  }
+                } else {
+                  $("#contact-us-form").slideUp( "slow");
+                  $("#contact-us-success").removeClass("hide-it")
+                                           .slideDown("slow");
+                  $("#success-message").html(data['message'])
+                }
+              }
+          });
+      });
+
+    }
   }
 
-  if (matchMedia('only screen and (min-width: 767px)').matches) {
-    $(document).click(function(e) {
-      // look to see if any skill-category elements are visible / open
-      // if they are close them!
-      $(".skill-text", this).css("display", "none");
-      $("#portfolio .skills-img").css("width", "40%")
-      $("#portfolio").css("opacity", "1");
-      $('.skill-info').addClass("hide-it"); 
-      $(this).removeClass("active");
-      $(".skill-category").removeClass("focused");
-      $("#portfolio").removeClass("focused");
-    });
-  }
+   /**
+  * initialise the website JavaScript once the DOM is ready
+  */
+  $(function() {
+    layout.init();
+  })
 
-});
+})(jQuery);
