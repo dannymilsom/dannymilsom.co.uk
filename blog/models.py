e4acc8eec97a074d06023e186c849272c0c06a7d
Danny Milsom <dannysmilsom@gmail.com>
2015-08-25 21:26:31 +0100

    General tidy up of models.py
    * Spaces not tabs
    * Added an ordering meta
    * Removed unused import

--- blog/models.py
+++ blog/models.py
@@ -1,35 +1,36 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.template.defaultfilters import slugify
 
 class Post(models.Model):
 
-	TAG_CHOICES = (
-		('Python', 'Python'),
-		('Linux', 'Linux'),
-		('HTML', 'HTML'),
-		('CSS', 'CSS'),
-		('Django', 'Django'),
-		('JavaScript', 'JavaScript'), 
-		('jQuery', 'jQuery'), 
-		('Web', 'Web'),
-		('Bootstrap', 'Bootstrap'),
-		('Apache HTTP', 'Apache HTTP'),
-		('PostgreSQL', 'PostgreSQL'),
-		('Backbone', 'Backbone'),
-	)
+    TAG_CHOICES = (
+        ('Python', 'Python'),
+        ('Linux', 'Linux'),
+        ('HTML', 'HTML'),
+        ('CSS', 'CSS'),
+        ('Django', 'Django'),
+        ('JavaScript', 'JavaScript'), 
+        ('jQuery', 'jQuery'), 
+        ('Web', 'Web'),
+        ('Bootstrap', 'Bootstrap'),
+        ('Apache HTTP', 'Apache HTTP'),
+        ('PostgreSQL', 'PostgreSQL'),
+        ('Backbone', 'Backbone'),
+    )
 
-	title = models.CharField(max_length=100)
-	summary = models.TextField(max_length=500)
-	message = models.TextField(max_length=1000)
-	date = models.DateField()
-	author = models.CharField(max_length=100)
-	slug = models.SlugField(max_length=40)
-	tags = models.CharField(max_length=20, choices=TAG_CHOICES,
-	                          default='Python')
+    class Meta:
+        ordering = ['-date']
 
-	def __unicode__(self):
-	    return self.title
+    title = models.CharField(max_length=100)
+    summary = models.TextField(max_length=500)
+    message = models.TextField(max_length=1000)
+    date = models.DateField()
+    author = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=40)
+    tags = models.CharField(max_length=20, choices=TAG_CHOICES, default='Python')
 
-	def get_absolute_url(self):
-		return reverse('blog.views.individual_post', kwargs={'slug': self.slug})
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog.views.individual_post', kwargs={'slug': self.slug})
