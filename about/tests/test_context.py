746ded96002df2fdc30ff4c6ab6967d0c0f67c57
Danny Milsom <dannysmilsom@gmail.com>
2015-09-20 21:01:15 +0100

    Tidy up of tests.

---
+++ about/tests/test_context.py
@@ -0,0 +1,19 @@
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
+
+
+class ContextProcessorTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_social_media_links_in_context(self):
+
+        resp = self.client.get(reverse('homepage'))
+        context = resp.context
+
+        self.assertIn('TWITTER_URL', context)
+        self.assertIn('GITHUB_URL', context)
+        self.assertIn('LINKEDIN_URL', context)
+        self.assertIn('STACKOVERFLOW_URL', context)
+        self.assertIn('INSTAGRAM_URL', context)
