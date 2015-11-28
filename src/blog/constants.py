3b2c4f9256f3ac6d3bc6bce17166d24871356e0d
Danny Milsom <dannysmilsom@gmail.com>
2015-11-28 18:04:16 +0000

    Tidy up blog model
    * Move tag categories to constants file
    * Some PEp-8 improvements

---
+++ src/blog/constants.py
@@ -0,0 +1,14 @@
+BLOG_POST_CATEGORIES = (
+    ('Python', 'Python'),
+    ('Linux', 'Linux'),
+    ('HTML', 'HTML'),
+    ('CSS', 'CSS'),
+    ('Django', 'Django'),
+    ('JavaScript', 'JavaScript'), 
+    ('jQuery', 'jQuery'), 
+    ('Web', 'Web'),
+    ('Bootstrap', 'Bootstrap'),
+    ('Apache HTTP', 'Apache HTTP'),
+    ('PostgreSQL', 'PostgreSQL'),
+    ('Backbone', 'Backbone'),
+)
--- src/blog/models.py
+++ src/blog/models.py
@@ -1,22 +1,11 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from .constants import BLOG_POST_CATEGORIES
+
 class Post(models.Model):
 
-    TAG_CHOICES = (
-        ('Python', 'Python'),
-        ('Linux', 'Linux'),
-        ('HTML', 'HTML'),
-        ('CSS', 'CSS'),
-        ('Django', 'Django'),
-        ('JavaScript', 'JavaScript'), 
-        ('jQuery', 'jQuery'), 
-        ('Web', 'Web'),
-        ('Bootstrap', 'Bootstrap'),
-        ('Apache HTTP', 'Apache HTTP'),
-        ('PostgreSQL', 'PostgreSQL'),
-        ('Backbone', 'Backbone'),
-    )
+    TAG_CHOICES = BLOG_POST_CATEGORIES
 
     class Meta:
         ordering = ['-date']
@@ -27,10 +16,17 @@ class Post(models.Model):
     date = models.DateField()
     author = models.CharField(max_length=100)
     slug = models.SlugField(max_length=40)
-    tags = models.CharField(max_length=20, choices=TAG_CHOICES, default='Python')
+    tags = models.CharField(
+        max_length=20,
+        choices=TAG_CHOICES,
+        default='Python',
+    )
 
     def __unicode__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog.views.individual_post', kwargs={'slug': self.slug})
+        return reverse(
+            'blog.views.individual_post',
+            kwargs={'slug': self.slug}
+        )
