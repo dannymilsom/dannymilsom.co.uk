ee8507601dfe6eec4a1005be4a9b188007577dd6
Danny Milsom <dannysmilsom@gmail.com>
2015-12-06 21:07:52 +0000

    Add script and source concat to gulp build process

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,62 @@
-// Plugin Dependenices
+// Core dependencies
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
+    path = require('path');
+
+// Gulp plugin dependencies
+var concat = require('gulp-concat'),
     minifycss = require('gulp-minify-css'),
     sass = require('gulp-sass');
 
-// Path Variables
-var scss_src = 'src/dannywebsite/static/dannywebsite/sass/**/*.scss',
-    scss_dst = 'src/dannywebsite/static/dannywebsite/css/';
+// Base static directories
+var static_dir = path.join('src', 'dannywebsite', 'static', 'dannywebsite'),
+    css_dir = path.join(static_dir, 'css');
+    js_dir = path.join(static_dir, 'js');
 
+// SASS paths
+var scss_src = path.join(static_dir, 'sass/**/*.scss'),
+    scss_dst = path.join(static_dir, 'css');
+
+// Compile SASS to CSS
 gulp.task('styles', function() {
     gulp.src(scss_src)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(scss_dst));
 });
 
+// Styles sources
+var styles_src = [
+    path.join(css_dir, 'vendor', 'bootstrap.min.css'),
+    path.join(css_dir, 'vendor', 'select2.css'),
+    path.join(css_dir, 'layout.css'),
+    path.join(css_dir, 'main.css'),
+]
+
+// Concat styles
+gulp.task('concat-styles', function() {
+    gulp.src(styles_src)
+    .pipe(concat({ path: 'dannymilsom.css'}))
+    .pipe(gulp.dest(css_dir));
+});
+
+// Script soruce paths
+var scripts_src = [
+    path.join(js_dir, 'vendor', 'jquery.min.js'),
+    path.join(js_dir, 'vendor', 'jquery-migrate-1.0.0.min.js'),
+    path.join(js_dir, 'vendor', 'select2.min.js'),
+    path.join(js_dir, 'vendor', 'bootstrap.min.js'),
+    path.join(js_dir, 'vendor', 'matchMedia.js'),
+    path.join(js_dir, 'layout.js'),
+]
+
+// Concat scripts
+gulp.task('concat', function() {
+    gulp.src(scripts_src)
+    .pipe(concat({ path: 'dannymilsom.js'}))
+    .pipe(gulp.dest(js_dir));
+});
+
 gulp.task('default',function() {
     gulp.watch(scss_src,['styles']);
 });
+
+gulp.task('build', ['styles', 'concat', 'concat-styles'])
