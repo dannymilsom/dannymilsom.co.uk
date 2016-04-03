e3089f95310f4d03552e3b48796f859918101b6c
Danny Milsom <dannysmilsom@gmail.com>
2016-04-03 08:48:22 +0100

    More styling of blog and landing page
    * Center align blog content
    * Remove blog images
    * Remove portfolio screen shots

--- src/about/templatetags/tags.py
+++ src/about/templatetags/tags.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.simple_tag
 def active(request, pattern):
-	"""
-	Used in templates to identify the current page by parsing the URL, 
-	returning active if the pattern matches the request path. 
-	"""
-	if re.search(pattern, request.path):
-		return 'active'
+    """
+    Used in templates to identify the current page by parsing the URL, 
+    returning active if the pattern matches the request path. 
+    """
+    if re.search(pattern, request.path):
+        return 'active'
